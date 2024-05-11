d27e65211978e7c50dba4b5e2be87855c5124014
Mojtaba Farzaneh <mojtabafarzaneh78@gmail.com>
2024-05-11 19:36:54 +0330

    partialy initializing the db

--- main.go
+++ main.go
@@ -1,13 +1,43 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/mojtabafarzaneh/hotel_reservation/api"
+	"github.com/mojtabafarzaneh/hotel_reservation/types"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const dburi = "mongodb://192.168.1.161:27017"
+const dbname = "hotel_reservation"
+const userColl = "users"
+
 func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().
+		ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+	coll := client.Database(dbname).Collection(userColl)
+
+	user := types.User{
+		FirstName: "moji",
+		LastName:  "farzaneh",
+	}
+	res, err := coll.InsertOne(ctx, user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(res)
+
 	listenAdder := flag.String("listenAdder", ":5000", "the listen address of the api server")
 	flag.Parse()
 	app := fiber.New()
