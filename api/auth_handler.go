31f6c1bbea0b81b222f46c2164a18af7096b861f
Mojtaba Farzaneh <mojtabafarzaneh78@gmail.com>
2024-06-03 20:36:02 +0330

    a bit of error handeling

--- api/auth_handler.go
+++ api/auth_handler.go
@@ -3,6 +3,7 @@ package api
 import (
 	"errors"
 	"fmt"
+	"net/http"
 	"os"
 	"time"
 
@@ -38,6 +39,17 @@ type AuthResponse struct {
 	User  *types.User `json:"user"`
 	Token string      `json:"token"`
 }
+type genericResp struct {
+	Type string `json:"type"`
+	Msg  string `json:"msg"`
+}
+
+func InvalidCredentials(c *fiber.Ctx) error {
+	return c.Status(http.StatusBadRequest).JSON(genericResp{
+		Type: "error",
+		Msg:  "invalid credentials",
+	})
+}
 
 func (h *AuthHandler) HandleAuthentication(c *fiber.Ctx) error {
 	var params AuthParams
@@ -48,12 +60,12 @@ func (h *AuthHandler) HandleAuthentication(c *fiber.Ctx) error {
 	user, err := h.userStore.GetUserByEmail(c.Context(), params.Email)
 	if err != nil {
 		if errors.Is(err, mongo.ErrNoDocuments) {
-			return fmt.Errorf("invalid credentials")
+			return InvalidCredentials(c)
 		}
 	}
 
 	if !types.IsValidPassword(user.EncryptedPassword, params.Password) {
-		return fmt.Errorf("invalid vredentials")
+		return InvalidCredentials(c)
 	}
 
 	resp := AuthResponse{
